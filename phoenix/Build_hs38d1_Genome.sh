91934fa2d91df8fb8b4f6ae7857a8ac376a14a55
PedalheadPHX <jkeats@tgen.org>
2019-03-07 11:09:56 -0700

    Updates and contig checks, still issues to resolve versus Broad Bundle and BWAKIT

--- phoenix/Build_hs38d1_Genome.sh
+++ phoenix/Build_hs38d1_Genome.sh
@@ -17,7 +17,17 @@
 
 # Confirm BWAKIT and Broad Bundle are identical in sequence
 diff /home/tgenref/homo_sapiens/grch38_hg38/hs38dh/genome_references/hs38DH.fa /home/tgenref/homo_sapiens/grch38_hg38/broad_resource_bundle/Homo_sapiens_assembly38.fasta
-# There seems to be differences
+# A massive number of events are seen BUT this seems to relate to the differences in the FASTA line lengths as the broad bundle lengths are longer 101 letters versus 71 (which we get with NCBI download as well)
+
+# Check contigs
+grep "^>" /home/tgenref/homo_sapiens/grch38_hg38/hs38dh/genome_references/hs38DH.fa > hs38DH_contigs.txt
+grep "^>" /home/tgenref/homo_sapiens/grch38_hg38/broad_resource_bundle/Homo_sapiens_assembly38.fasta > Homo_sapiens_assembly38_contigs.txt
+
+diff hs38DH_contigs.txt Homo_sapiens_assembly38_contigs.txt
+# No contig differences
+
+# Download GRC/NCBI README
+wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids/README_analysis_sets.txt
 
 ####################################
 ## BUILD BWA REFERENCE GENOME
@@ -47,8 +57,26 @@ awk '/HLA-A\*01:01:01:01/{print FNR}' bwa.kit/resource-GRCh38/hs38DH-extra.fa
 awk 'NR>=75967' bwa.kit/resource-GRCh38/hs38DH-extra.fa > bwakit_HLA.fasta
 
 ### Concatenate the fasta files to make final reference genome fasta
-cat GCA_000001405.15_GRCh38_full_plus_hs38d1_analysis_set.fna bwakit_HLA.fasta > GRCh38_hs38d1_HLA.fa
+cat GCA_000001405.15_GRCh38_full_plus_hs38d1_analysis_set.fna bwakit_HLA.fasta > GRCh38_hs38d1_Alts_HLA.fa
+
+# Get a contig list for comparisons
+grep "^>" GRCh38_hs38d1_Alts_HLA.fa > GRCh38_hs38d1_Alts_HLA_contigs.txt
+diff GRCh38_hs38d1_Alts_HLA_contigs.txt hs38DH_contigs.txt
+diff GRCh38_hs38d1_Alts_HLA_contigs.txt Homo_sapiens_assembly38_contigs.txt
+# Lots of differences in contigs, not sure why
 
 ### Check versus HS38DH from BWAKIT
-diff GRCh38_hs38d1_HLA.fa /home/tgenref/homo_sapiens/grch38_hg38/hs38dh/genome_references/hs38DH.fa
+diff GRCh38_hs38d1_Alts_HLA.fa /home/tgenref/homo_sapiens/grch38_hg38/hs38dh/genome_references/hs38DH.fa
 # Differences with repeat masked lowecase and uppercase AGCT on the decoy contigs
+diff GRCh38_hs38d1_Alts_HLA.fa /home/tgenref/homo_sapiens/grch38_hg38/broad_resource_bundle/Homo_sapiens_assembly38.fasta
+
+####################################
+## BUILD STAR REFERENCE GENOME
+####################################
+
+# Since start does not support alternative contigs, download version wtih out
+wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna.gz
+gunzip GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna.gz
+
+# Rename fastq file
+mv GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna GRCh38_hs38d1.fa
