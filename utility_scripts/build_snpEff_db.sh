58b307ac49eb6d51ae137e4485d2f6cbb773b310
PedalheadPHX <jkeats@tgen.org>
2019-08-12 20:54:34 -0700

    debug update to support dynamic snpEff.config

--- utility_scripts/build_snpEff_db.sh
+++ utility_scripts/build_snpEff_db.sh
@@ -13,7 +13,8 @@ module load snpEff/${SNPEFF_VERSION}
 
 # Call snpEff build, normally this would be java -jar snpEff but the module load points to a wrapper script
 # With module load snpEff/v4_3t
-$ snpEff == java -jar snpEff.jar
+# snpEff == java -jar snpEff.jar
+
 snpEff build -gtf22 -v ${SNPEFF_DB_NAME} -c ${SNPEFF_CONFIG_PATH}
 
 touch CREATED_SNPEFF_${SNPEFF_DB_NAME}_DATABASE
