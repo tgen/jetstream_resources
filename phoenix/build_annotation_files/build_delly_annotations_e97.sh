174be9ce1c72f7407b34cb82e4469e685efdc956
ChristopheLegendre <clegendre@tgen.org>
2019-10-02 16:36:41 -0700

    update build_delly_annotations and added python scripts in utility dir

--- phoenix/build_annotation_files/build_delly_annotations_e97.sh
+++ phoenix/build_annotation_files/build_delly_annotations_e97.sh
@@ -1,1 +1,79 @@
-#Christophe to add
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+function make_file_list_biotypes_to_keep(){
+local FILE_OUT=$1
+## List of Biotypes to KEEP in the Final GTF
+echo -e "IG_C_gene
+IG_C_pseudogene
+IG_D_gene
+IG_J_gene
+IG_J_pseudogene
+IG_V_gene
+IG_V_pseudogene
+lincRNA
+miRNA
+Mt_rRNA
+Mt_tRNA
+processed_transcript
+protein_coding
+rRNA
+snoRNA
+snRNA
+TR_C_gene
+TR_D_gene
+TR_J_gene
+TR_J_pseudogene
+TR_V_gene
+TR_V_pseudogene" > ${FILE_OUT}
+}
+
+ENSEMBL_VERSION=97
+DIR_IN=/home/tgenref/homo_sapiens/grch38_hg38/hg38tgen/gene_model/ensembl_v${ENSEMBL_VERSION/
+EXPECTED_GTF_FILE=${DIR_IN}/Homo_sapiens.GRCh38.${ENSEMBL_VERSION.ucsc.gtf
+DIR_OUT=/home/tgenref/homo_sapiens/grch38_hg38/hg38tgen/tool_resources/delly/ensembl_v${ENSEMBL_VERSION}/
+
+
+COUNT_GTF_FILES_FOUND=$(find $(dirname ${EXPECTED_GTF_FILE}) -type f -name "$(basename ${EXPECTED_GTF_FILE})" | wc -l)
+if [[ ${COUNT_GTF_FILES_FOUND} -eq 1 ]] ; 
+then 
+	GTF_FILE=$(find $(dirname ${EXPECTED_GTF_FILE}) -type f -name "$(basename ${EXPECTED_GTF_FILE})")
+else
+	echo -e "ERROR: FNF:  Expected file << ${EXPECTED_GTF_FILE} >>  NOT FOUND" 
+	exit 1
+fi
+
+if [[ ! -e ../../utility_scripts/parse_gtf.py && ! -e ../../utility_scripts/GTF.py ]]
+then
+	echo -e "ERROR: Missing Python scripts; Scripts parse_gtf and GTF.py scripts MUST be in << utility_scripts >> directory "
+	exit 1
+fi
+
+## definition of the output files here:
+mkdir -p ${DIR_OUT}
+FILE_LIST_BIOTYPES=${DIR_OUT}/list_biotypes_to_keep_for_grep.txt
+make_file_list_biotypes_to_keep ${FILE_LIST_BIOTYPES}
+INTERMEDIATE_GTF=${DIR_OUT}/${GTF_FILE}_gene.gtf
+BED_ANNOFILE_ALL_BIOTYPES=${DIR_OUT}/delly_anno_$(basename ${GTF_FILE%.*}).all_biotypes.bed
+BED_ANNOFILE_KEPT_BIOTYPES=${DIR_OUT}/delly_anno_$(basename ${GTF_FILE%.*}).bed
+
+
+
+echo -e "Extracting lines from GTF with gene information only ..."
+cat ${GTF_FILE} | awk -F"\t" ' $3=="gene" { OFS="\t" ; print} ' > ${INTERMEDIATE_GTF}
+if [[ $? -ne 0 ]] ;	then echo -e "ERROR: Subset GTF by GENE only FAILED; Aborting." ; exit 1 ; fi
+
+
+echo -e "parsing GTF ... and making all_biotype bed file ..."
+python ../../utility_scripts/parse_gtf.py --gtf ${INTERMEDIATE_GTF} --out ${BED_ANNOFILE_ALL_BIOTYPES}
+echo -e "subsetting by specific biotypes ..."
+cat ${BED_ANNOFILE_ALL_BIOTYPES} | grep -wf ${FILE_LIST_BIOTYPES}  > ${BED_ANNOFILE_KEPT_BIOTYPES}
+if [[ $? -ne 0 ]] ;	then echo -e "ERROR: grep FAILED ;Aborting ;" ; exit 1 ; fi
+
+echo -e "Cleaning temporary files ..."
+rm ${BED_ANNOFILE_ALL_BIOTYPES} ${INTERMEDIATE_GTF} ${FILE_LIST_BIOTYPES}
+
+echo -e "Build Anno File for Delly: DONE"
+exit
+
---
+++ utility_scripts/parse_gtf.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging as log
+import shutil
+import sys
+
+import GTF
+
+
+
+class UniqueStore(argparse.Action):
+	"""
+	class grabbed from stackOverflow (2018-11-08)
+	https://stackoverflow.com/questions/23032514/argparse-disable-same-argument-occurences
+	Thanks To the Community
+	We override the function __call__ from argparse to check if one option is given more than once
+	"""
+
+	def __call__(self, parser, namespace, values, option_string):
+		if getattr(namespace, self.dest, self.default) is not None:
+			parser.error(option_string + " appears several times.  Please modify your options.")
+		setattr(namespace, self.dest, values)
+
+def check_if_executable_in_path(list_executables):
+	for executable in list_executables:
+		if shutil.which(executable) is None:
+			sys.exit(str(executable) + "  NOT IN PATH ; Aborting;")
+
+
+def make_parser_args():
+	parser = argparse.ArgumentParser()
+	parser._action_groups.pop()
+	required = parser.add_argument_group('required arguments')
+	optional = parser.add_argument_group('optional arguments')
+	isRequired = True
+
+	required.add_argument('--gtf',
+	                      required=isRequired,
+	                      action=UniqueStore,
+	                      help='URL to download GTF file or full path to a GTF (can be compressed or not)')
+	required.add_argument('--out',
+	                      required=True,
+	                      action=UniqueStore,
+	                      help='name of the output file')
+
+	return parser
+
+
+def main(args, cmdline):
+	p = GTF.dataframe(args['gtf'])  ## dataframe returns a pandas.core.data.DataFrame
+	with open(args['out'], 'w') as wo:
+		for i in range(len(GTF.dataframe(args['gtf']))):
+			wo.write("{}\t{}\t{}\t{}___{}\t{}\t{}\n".format(p['seqname'][i], p['start'][i], p['end'][i], p['gene_id'][i], p['gene_name'][i],p['gene_biotype'][i], p['strand'][i]))
+
+if __name__ == '__main__':
+
+	FORMAT_LOGGING = '%(levelname)s %(asctime)-15s %(module)s %(lineno)d\t %(message)s'
+	log.basicConfig(format=FORMAT_LOGGING, level=log.INFO)
+	list_executables = ['python3']  ## list of executables required to be in path
+	check_if_executable_in_path(list_executables)
+
+	##TODO check if ALL intended python packages are present in python3
+
+	# capture command line
+	cmdline = ' '.join(sys.argv)
+	parser = make_parser_args()
+	args = vars(parser.parse_args())  # vars() function returns a dictionary of key-value arguments
+	log.info(str(args))
+	log.info(' '.join(["Command Line captured: ", cmdline]))
+	try:
+		main(args, cmdline)
+	except Exception as e:
+		log.error("ERROR: Exception got Raised; Check you inputs and/or options ; {}".format(e))
+		sys.exit(1)
+	sys.exit()
+
