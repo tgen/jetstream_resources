f19014c34fe97482dd7a2df5444a98c7f81b7142
PedalheadPHX <jkeats@tgen.org>
2019-11-05 15:01:16 -0700

    removing test code

--- phoenix/build_annotation_files/build_lymphocyte_count_windows.sh
+++ phoenix/build_annotation_files/build_lymphocyte_count_windows.sh
@@ -37,20 +37,3 @@ echo -e chr14"\t"105583730"\t"106879812"\t"IGH >> lymphocyteReceptor_loci.bed
 echo -e chr22"\t"22030933"\t"22923034"\t"IGL >> lymphocyteReceptor_loci.bed
 echo -e chr14"\t"21621837"\t"22552156"\t"TRA >> lymphocyteReceptor_loci.bed
 echo -e chr7"\t"142299176"\t"142802748"\t"TRB >> lymphocyteReceptor_loci.bed
-
-# Exit script
-exit 0
-
-### Count testing
-
-## both samtools cov and bedtools coverage seems slow, neither multithreaded
-module load samtools/1.9
-
-# samtools works but slow, single thread, memory at 0.1
-samtools bedcov /scratch/jkeats/lymphocyte_count_windows.bed MMRF_1364_1_BM_CD138pos_T3_TSMRU.star.bam > /scratch/jkeats/test.txt
-
-#bedtools coverage - reading a BAM file is using significant memory, actually got KILLED by Slurm
-/home/jkeats/downloads/bedtools2/bin/bedtools coverage -split -counts -a MMRF_1364_1_BM_CD138pos_T3_TSMRU.star.bam -b /scratch/jkeats/lymphocyte_count_windows.bed > /scratch/jkeats/bedtools_counts.txt
-
-# old like approach, killed, hit memory above 20G as srun request for 10 cores
-samtools view --threads 8 -b MMRF_1364_1_BM_CD138pos_T3_TSMRU.star.bam | /home/jkeats/downloads/bedtools2/bin/bedtools coverage -split -counts -abam stdin -b /scratch/jkeats/lymphocyte_count_windows.bed  > /scratch/jkeats/bedToolsIglCount.txt
