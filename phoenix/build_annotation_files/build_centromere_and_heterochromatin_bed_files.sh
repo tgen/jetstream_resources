9ca730c40ed6785e33f32229828c3cfe33761b71
awchrist <achristofferson@tgen.org>
2020-02-18 18:36:32 -0700

    Added the centromere merged with the encode black list

--- phoenix/build_annotation_files/build_centromere_and_heterochromatin_bed_files.sh
+++ phoenix/build_annotation_files/build_centromere_and_heterochromatin_bed_files.sh
@@ -2,6 +2,8 @@
 
 # Usage: ./build_centromere_and_heterochromatin_bed_files.sh  <resources.ini>
 
+module load BEDTools/2.29.0-GCC-8.2.0-2.31.1
+
 ## This file is created for usage in jetstream_build_package
 
 # Check if resources.ini was provided on the command line
@@ -25,7 +27,6 @@ mkdir -p public_databases/ncbi
 cd public_databases/ncbi
 
 # Download the Modeled centromeres and heterochromatin regions
-
 wget https://www.ncbi.nlm.nih.gov/projects/genome/assembly/grc/human/data/38/Modeled_regions_for_GRCh38.tsv
 
 # Make a bed file of all of the Modeled_regions_for_GRCh38
@@ -36,6 +37,19 @@ grep CEN Modeled_regions_for_GRCh38.bed > Modeled_regions_for_GRCh38_centromere.
 
 # Make heterochromitin only bed file
 grep HET Modeled_regions_for_GRCh38.bed > Modeled_regions_for_GRCh38_heterochromitin.bed
+
+# Get the regions of the black list that intersect with the centromeres
+bedtools intersect -wb -a Modeled_regions_for_GRCh38_centromere.bed \
+	-b /home/tgenref/homo_sapiens/grch38_hg38/public_databases/encode/Blacklist-2.0/lists/hg38-blacklist.v2.bed.gz | \
+	cut -f6- | \
+	sort -k1,1V -k2,2n -k3,3n > Modeled_regions_for_GRCh38_centromere_blacklist_intersect.bed
+
+# Merge the centromere regions and the overlaping encode blacklist regions together
+cat Modeled_regions_for_GRCh38_centromere.bed \
+	Modeled_regions_for_GRCh38_centromere_blacklist_intersect.bed | \
+	cut -f1-4 | \
+	sort -k1,1V -k2,2n -k3,3n | \
+	bedtools merge > Modeled_regions_for_GRCh38_centromere_blacklist_merged.bed
 
 # Write this full document as a README
 cat $0 > README
