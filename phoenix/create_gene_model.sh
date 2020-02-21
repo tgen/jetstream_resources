cab9df5623d603a2d360e567cc2b2276325a8759
awchrist <achristofferson@tgen.org>
2020-02-21 08:07:59 -0700

    Added bed files for per gene and per exon to create_gene_model.sh

--- phoenix/create_gene_model.sh
+++ phoenix/create_gene_model.sh
@@ -229,6 +229,19 @@ else
 fi
 echo >> README
 
+# Create a bed file for the start and stop for each gene
+awk -F '[\t"]' '$1 !~ /^#/ { if (a[$10] == "" ) { a[$10] = $1 ; b[$10] = $4 ; c[$10] = $5 ; next } ;
+        if ($4 < b[$10]) { b[$10] = $4 } ;
+        if ($5 > c[$10]) { c[$10] = $5 }
+} END {
+for (i in a) {
+        OFS = "\t" ; print a[i], b[i], c[i], i 
+}
+}' ${GTF_FILE_BASE}.ucsc.gtf | sort -k1,1V -k2,2n -k3,3n > ${GTF_FILE_BASE}.ucsc.gene.bed
+
+# Create a bed file for the start and stop of each exon for each gene
+awk -F '[\t"]' '$1 !~ /^#/ { if ($3 == "exon") { OFS = "\t" ; print $1, $4, $5, $10 }}' ${GTF_FILE_BASE}.ucsc.gtf | sort -k1,1V -k2,2n -k3,3n > ${GTF_FILE_BASE}.ucsc.exon.bed 
+
 # Create reflat file from GTF for Picard RNAseqMetrics
 # Uses gtfToGenePred from UCSC
 # http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/gtfToGenePred
