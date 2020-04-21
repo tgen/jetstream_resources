da1ed0600d24bb5ef7f4453dec067df996010747
PedalheadPHX <jkeats@tgen.org>
2020-04-20 20:47:41 -0700

    improved dynamic configuration

--- phoenix/build_annotation_files/build_cosmic_v90.sh
+++ phoenix/build_annotation_files/build_cosmic_v90.sh
@@ -4,8 +4,90 @@
 
 # Usage: ./build_cosmic_v90.sh
 
-# Write this full document as a README
-cat $0 > README
+### Setting as an interactive BASH session and forcing history to capture commands to a log/README file
+HISTFILE=~/.bash_history
+set -o history
+set -ue
+
+# Check resources.ini was provided on the command line
+if [ -n "$1" ]
+then
+  echo "Required ini file detected"
+else
+  echo "Input INI file not provided, exiting due to missing requirement"
+  exit 1
+fi
+
+# Read required variables from configuration file
+. ${1}
+
+
+
+if [ $ENVIRONMENT == "TGen"]
+then
+
+####################################
+## Load Required Tools
+###################################
+module load BCFtools/1.10.1-foss-2019a
+
+else
+  echo
+  echo "Assuming required tools are available in $PATH"
+  echo
+fi
+
+####################################
+## Create Expected Folder Structure
+###################################
+
+# Make top level directory if not available
+if [ -e ${PARENT_DIR} ]
+then
+    echo "Parent directory: ${PARENT_DIR} exists, moving into it"
+    cd ${PARENT_DIR}
+else
+    echo "Parent directory NOT fount, creating and moving into it now"
+    mkdir -p ${PARENT_DIR}
+    cd ${PARENT_DIR}
+fi
+
+# Make public_databases folder if not available
+if [ -e public_databases ]
+then
+    echo "Public Databases folder exists, moving into it"
+    cd public_databases
+else
+    echo "Public Databases folder NOT fount, creating and moving into it now"
+    mkdir -p public_databases
+    cd public_databases
+fi
+
+# Make cosmic folder if not available
+if [ -e cosmic ]
+then
+    echo "COSMIC folder exists, moving into it"
+    cd cosmic
+else
+    echo "COSMIC folder NOT fount, creating and moving into it now"
+    mkdir -p cosmic
+    cd cosmic
+fi
+
+# Make COSMIC release version folder if not available
+if [ -e ${COSMIC_VERSION} ]
+then
+    echo "dbSNP ${COSMIC_VERSION} folder exists, exiting to prevent overwrite"
+    exit 1
+else
+    echo "dbSNP ${COSMIC_VERSION} folder NOT fount, creating and moving into it now"
+    mkdir -p ${COSMIC_VERSION}
+    cd ${COSMIC_VERSION}
+fi
+
+###################################
+## Download and process the coding mutation VCF file
+###################################
 
 # Added user information and timestamp to README
 USER=`whoami`
@@ -13,25 +95,13 @@ DATE=`date`
 echo "Downloaded and Processed by:  ${USER}" >> README
 echo ${DATE} >> README
 
-# Modules needed
-module load samtools/1.9
-
-# Define FAI file
-FAI=/home/tgenref/homo_sapiens/grch38_hg38/hg38tgen/genome_reference/GRCh38tgen_decoy_alts_hla.fa.fai
-
 # Generate authentication token
-EMAIL=jkeats@tgen.org
+#EMAIL=jkeats@tgen.org
 #PASSWORD=NotMyRealPassword123
 echo "Please enter your COSMIC website authentication password"
 read PASSWORD
 
-TOKEN=`echo "${EMAIL}:${PASSWORD}" | base64`
-
-############################
-###
-### Download and process the coding mutation VCF file
-###
-############################
+TOKEN=`echo "${COSMIC_ACCOUNT_EMAIL}:${PASSWORD}" | base64`
 
 ## DOWNLOAD Coding Mutations file
 # Get download link, this generates a custom url and access key for the next step, returned as a JSON, which is parsed into the download key
@@ -61,15 +131,21 @@ bcftools index --tbi CosmicCodingMuts_v90_GRCh38.vcf.bgz
 # Update the contig names using bcftools
 bcftools annotate \
 	--threads 4 \
-	--rename-chrs /home/jkeats/git_repositories/GRCh38_CrossMapping/GRCh38_Cosmic89_2_UCSC_Contigs.txt \
+	--rename-chrs ${PATH_TO_REPO}/utility_files/GRCh38_Cosmic90_2_UCSC_Contigs.txt \
 	--output-type b \
 	--output temp_renamed.bcf \
 	CosmicCodingMuts_v90_GRCh38.vcf.bgz
 bcftools index --threads 4 temp_renamed.bcf
 
+# Determine the full name and path of the DNA genome.fa.fai index file
+echo "Determine the full path filename of the DNA reference genome.fa.fai file" >> README
+echo "REFERENCE_DNA_GENOME_FAI=${TOPLEVEL_DIR}/genome_reference/${REFERENCE_DNA_GENOME_NAME}.fai" >> README
+REFERENCE_DNA_GENOME_FAI=${TOPLEVEL_DIR}/genome_reference/${REFERENCE_DNA_GENOME_NAME}.fai
+echo >> README
+
 # Add updated contig header lines and add missing ones, required current bcftools version
-/home/jkeats/downloads/bcftools/bcftools reheader \
-	--fai ${FAI} \
+bcftools reheader \
+	--fai ${REFERENCE_DNA_GENOME_FAI} \
 	temp_renamed.bcf \
 	| \
 	bcftools view \
@@ -114,15 +190,15 @@ bcftools index --tbi CosmicNonCodingMuts_v90_GRCh38.vcf.bgz
 # Update the contig names using bcftools
 bcftools annotate \
 	--threads 4 \
-	--rename-chrs /home/jkeats/git_repositories/GRCh38_CrossMapping/GRCh38_Cosmic89_2_UCSC_Contigs.txt \
+	--rename-chrs ${PATH_TO_REPO}/utility_files/GRCh38_Cosmic90_2_UCSC_Contigs.txt \
 	--output-type b \
 	--output temp_renamed.bcf \
 	CosmicNonCodingMuts_v90_GRCh38.vcf.bgz
 bcftools index --threads 4 temp_renamed.bcf
 
 # Add updated contig header lines and add missing ones, required current bcftools version
-/home/jkeats/downloads/bcftools/bcftools reheader \
-	--fai ${FAI} \
+bcftools reheader \
+	--fai ${REFERENCE_DNA_GENOME_FAI} \
 	temp_renamed.bcf \
 	| \
 	bcftools view \
@@ -135,9 +211,5 @@ rm CosmicNonCodingMuts_v90_GRCh38.vcf.bgz.tbi
 rm CosmicNonCodingMuts_v90_GRCh38.vcf.bgz
 rm CosmicNonCodingMuts_v90_GRCh38.vcf
 
-############################
-###
-### Download and Processing Notes
-###
-############################
+
 
