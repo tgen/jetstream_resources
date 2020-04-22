16803e21fc8b860fe71d836dfb2d76d9023a4983
PedalheadPHX <jkeats@tgen.org>
2020-04-21 23:14:59 -0700

    updated for local compute usage

--- utility_scripts/build_snpEff_db.sh
+++ utility_scripts/build_snpEff_db.sh
@@ -9,11 +9,20 @@
 
 # Usage: sbatch --export ALL,SNPEFF_VERSION="v4_3t",SNPEFF_DB_NAME="grch38.97",SNPEFF_CONFIG_PATH="/home/snpEff.config" build_snpEff_db.sh
 
+if [ $ENVIRONMENT == "TGen" ]
+then
+  # Load required module
+  module load snpEff/${SNPEFF_VERSION}
+else
+  # Set variables from inputs
+  ENVIRONMENT=${1}
+  SNPEFF_VERSION=${2}
+  SNPEFF_DB_NAME=${3}
+  SNPEFF_CONFIG_PATH=${4}
+  echo "Assuming required tools are available in $PATH"
+fi
 
-module load snpEff/${SNPEFF_VERSION}
-
-# Call snpEff build, normally this would be java -jar snpEff but the module load points to a wrapper script
-# With module load snpEff/v4_3t
+# Call snpEff build, normally this would be java -jar snpEff but the module load points to a wrapper script with module load snpEff/v4_3t
 # snpEff == java -jar snpEff.jar
 
 snpEff build -gtf22 -v ${SNPEFF_DB_NAME} -c ${SNPEFF_CONFIG_PATH}
