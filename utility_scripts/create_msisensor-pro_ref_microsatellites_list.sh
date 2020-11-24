9f0c2a88e28de80c041d2817320666816f4dd802
denriquez <denriquez@tgen.org>
2020-11-23 22:03:39 -0700

    Added MSIsensor-pro scan scripts to create MS list

---
+++ utility_scripts/create_msisensor-pro_ref_microsatellites_list.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name="MSIsensorPro_scan"
+#SBATCH --time=0-24:00:00
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH --cpus-per-task 2
+#SBATCH --partition defq,overflow
+
+## Script to create list of homopolymers and miscrosatelites from reference genome
+
+## Usage:
+## sbatch --export ALL,FASTA="genome.fa",MICROSATELLITES_LIST="GRCh38tgen_decoy_alts_hla.microsatellites.list",MSISENSOR_PRO_VERSION="1.1.a" create_msisensor-pro_ref_microsatellites_list.sh 
+
+module load msisensor/${MSISENSOR_PRO_VERSION}
+
+msisensor-pro scan -d "$FASTA" -o "$MICROSATELLITES_LIST"
+
+# Error Capture
+if [ "$?" = "0" ]
+then
+    echo "PASSED_MSISENSOR_PRO_SCAN" >> README
+else
+    touch FAILED_MSISENSOR_PRO_SCAN
+    echo "FAILED_MSISENSOR_PRO_SCAN" >> README
+    exit 1
+fi
